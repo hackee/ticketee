c11a760f35f45e1c16e02b083b66f4198ce9835d
hackee <hackee@163.com>
2011-10-14 23:19:57 +0800

    add project skelon

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketee::Application.config.secret_token = '6590edb443e67d720dd3e8fa378b5f2b4f5ba880a31afa7eec8126db03eac2ba8df28fcdf0d771d4c2938e5ea08f48e63f2d5a69ef571adb80fa36e40ba14235'
